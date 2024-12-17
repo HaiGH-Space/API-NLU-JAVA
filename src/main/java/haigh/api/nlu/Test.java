1e03d879f11c39c65596b475ae4df51f3a88a7c4
HaiGH <nthai0932169055@gmail.com>
2024-12-17 18:52:49 +0700

    update test

--- src/main/java/haigh/api/nlu/Test.java
+++ src/main/java/haigh/api/nlu/Test.java
@@ -1,10 +1,40 @@
 package haigh.api.nlu;
 
 
+import com.google.gson.Gson;
+import haigh.api.nlu.util.ConstantUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Test {
     public static void main(String[] args) {
         API api = new API();
+        Account myAccount = readAccountJSON();
+        System.out.println(api.login(myAccount.username,myAccount.password));
+    }
+    private static Account readAccountJSON() {
+        InputStream inputStream = ConstantUtil.class.getClassLoader().getResourceAsStream("account.json");
+        if (inputStream != null) {
+            String json = "";
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    json += line;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            Gson gson = new Gson();
+            return gson.fromJson(json, Account.class);
+        }
+        return null;
+    }
+    class Account{
+        public String username;
+        public String password;
     }
 }
